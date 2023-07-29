164a41285cc4dc93ec18b7814eebcdd6575636a0
go-ai-agent <mark.wilsonai@outlook.com>
2023-07-29 08:19:01 -0500

    Update origin.go

--- runtime/origin.go
+++ runtime/origin.go
@@ -23,6 +23,10 @@ func (o *Origin) String() string {
 	return fmt.Sprintf("%s:%s:%s:%s:%s", o.Region, o.Zone, o.SubZone, o.Service, o.InstanceId)
 }
 
+func (o *Origin) String2() string {
+	return fmt.Sprintf(OriginRegionKey+"%s:%s:%s:%s:%s", o.Region, o.Zone, o.SubZone, o.Service, o.InstanceId)
+}
+
 func NewOrigin(s string) *Origin {
 	o := new(Origin)
 	o.Region = s
