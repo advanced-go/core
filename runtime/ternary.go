d0cb2268387f22e2e5bdcb75939729cec35b9b93
go-ai-agent <mark.wilsonai@outlook.com>
2023-10-26 09:29:28 -0500

    Removed

--- runtime/ternary.go
+++
@@ -1,9 +0,0 @@
-package runtime
-
-// IfElseOp - templated function implementation of "C" ternary operator : conditional ? [true value] : [false value]
-func IfElseOp[T any](cond bool, trueT T, falseT T) T {
-	if cond {
-		return trueT
-	}
-	return falseT
-}
