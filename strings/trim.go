64cec7494dbb7e6bc8e5a4d84fa56af37920811f
go-ai-agent <mark.wilsonai@outlook.com>
2023-10-27 06:40:37 -0500

    Update trim.go

--- strings/trim.go
+++ strings/trim.go
@@ -5,3 +5,10 @@ import "strings"
 func TrimSpace(s string) string {
 	return strings.TrimRight(strings.TrimLeft(s, " "), " ")
 }
+
+func IsEmpty(s string) bool {
+	if s == "" {
+		return true
+	}
+	return strings.TrimLeft(s, " ") == ""
+}
