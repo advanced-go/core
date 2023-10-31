909c08dca000ba6560b57f94718a2ab3940a990c
go-ai-agent <mark.wilsonai@outlook.com>
2023-10-31 06:34:41 -0500

    Refactoring JSON marshalling

--- json/marshal.go
+++ json/marshal.go
@@ -5,15 +5,24 @@ import (
 	"github.com/go-ai-agent/core/runtime"
 )
 
-func MarshalType(t any) ([]byte, *runtime.Status) {
+func Marshal(t any) ([]byte, *runtime.Status) {
 	buf, err := json.Marshal(t)
 	if err != nil {
-		return nil, runtime.NewStatusError(runtime.StatusJsonEncodeError, runtime.PkgUri+"/MarshalType", err)
+		return nil, runtime.NewStatusError(runtime.StatusJsonEncodeError, runtime.PkgUri+"/Marshal", err)
 	}
 	return buf, runtime.NewStatusOK()
 }
 
-func UnmarshalType[T any](buf []byte) (T, *runtime.Status) {
+func Unmarshal(buf []byte, t any) *runtime.Status {
+	err := json.Unmarshal(buf, t)
+	if err != nil {
+		return runtime.NewStatusError(runtime.StatusJsonDecodeError, runtime.PkgUri+"/Unmarshal", err)
+	}
+	return runtime.NewStatusOK()
+}
+
+/*
+func Unmarshal[T any](buf []byte) (T, *runtime.Status) {
 	var t T
 
 	err := json.Unmarshal(buf, &t)
@@ -22,3 +31,6 @@ func UnmarshalType[T any](buf []byte) (T, *runtime.Status) {
 	}
 	return t, runtime.NewStatusOK()
 }
+
+
+*/
--- json/marshal_test.go
+++ json/marshal_test.go
@@ -2,61 +2,77 @@ package json
 
 import "fmt"
 
-func Example_MarshalType() {
+func Example_Marshal() {
 	var i = 45
-	buf, status := MarshalType(i)
-	fmt.Printf("test: MarshalType(int) [status:%v] %v\n", status, string(buf))
+	buf, status := Marshal(i)
+	fmt.Printf("test: Marshal(int) [status:%v] %v\n", status, string(buf))
 
 	var str []string
-	buf, status = MarshalType(str)
-	fmt.Printf("test: MarshalType([]string) [status:%v] %v\n", status, string(buf))
+	buf, status = Marshal(str)
+	fmt.Printf("test: Marshal([]string) [status:%v] %v\n", status, string(buf))
 
 	var ptr *int
-	buf, status = MarshalType(ptr)
-	fmt.Printf("test: MarshalType(*int(nil)) [status:%v] %v\n", status, string(buf))
+	buf, status = Marshal(ptr)
+	fmt.Printf("test: Marshal(*int(nil)) [status:%v] %v\n", status, string(buf))
 
 	ptr = &i
-	buf, status = MarshalType(ptr)
-	fmt.Printf("test: MarshalType(*int) [status:%v] %v\n", status, string(buf))
+	buf, status = Marshal(ptr)
+	fmt.Printf("test: Marshal(*int) [status:%v] %v\n", status, string(buf))
 
 	//Output:
-	//test: MarshalType(int) [status:OK] 45
-	//test: MarshalType([]string) [status:OK] null
-	//test: MarshalType(*int(nil)) [status:OK] null
-	//test: MarshalType(*int) [status:OK] 45
+	//test: Marshal(int) [status:OK] 45
+	//test: Marshal([]string) [status:OK] null
+	//test: Marshal(*int(nil)) [status:OK] null
+	//test: Marshal(*int) [status:OK] 45
 
 }
 
-func Example_UnmarshalType() {
+func Example_Unmarshal() {
 	var i = 45
-	buf, status := MarshalType(i)
+	buf, status := Marshal(i)
 	if status != nil {
 	}
 
-	j, status1 := UnmarshalType[int](buf)
-	fmt.Printf("test: UnmarshalType(int) [status:%v] %v\n", status1, j)
+	var j int
+	status = Unmarshal(buf, &j)
+	fmt.Printf("test: Unmarshal(int) [status:%v] %v\n", status, j)
 
 	var str = []string{"test", "of", "[]string"}
-	buf, status = MarshalType(str)
-	strs, status2 := UnmarshalType[[]string](buf)
-	fmt.Printf("test: UnmarshalType([]string) [status:%v] %v\n", status2, strs)
+	var str2 []string
+
+	buf, status = Marshal(str)
+	status = Unmarshal(buf, &str2)
+	fmt.Printf("test: Unmarshal([]string) [status:%v] %v\n", status, str2)
+
+	//fmt.Printf("test: Marshal([]string) [status:%v] %v\n", status, string(buf))
+
+	//Output:
+	//test: Unmarshal(int) [status:OK] 45
+	//test: Unmarshal([]string) [status:OK] [test of []string]
+
+}
+
+/*
+func Example_Unmarshal() {
+	var i = 45
+	buf, status := Marshal(i)
+	if status != nil {
+	}
+
+	j, status1 := Unmarshal[int](buf)
+	fmt.Printf("test: Unmarshal(int) [status:%v] %v\n", status1, j)
+
+	var str = []string{"test", "of", "[]string"}
+	buf, status = Marshal(str)
+	strs, status2 := Unmarshal[[]string](buf)
+	fmt.Printf("test: Unmarshal([]string) [status:%v] %v\n", status2, strs)
 
 	//fmt.Printf("test: MarshalType([]string) [status:%v] %v\n", status, string(buf))
 
-	/*
-		var ptr *int
-		buf, status = MarshalType[DebugError](ptr)
-		fmt.Printf("test: MarshalType(*int(nil)) [status:%v] %v\n", status, string(buf))
-
-		ptr = &i
-		buf, status = MarshalType[DebugError](ptr)
-		fmt.Printf("test: MarshalType(*int) [status:%v] %v\n", status, string(buf))
-
-
-	*/
-
 	//Output:
-	//test: UnmarshalType(int) [status:OK] 45
-	//test: UnmarshalType([]string) [status:OK] [test of []string]
+	//test: Unmarshal(int) [status:OK] 45
+	//test: Unmarshal([]string) [status:OK] [test of []string]
 
 }
+
+*/
