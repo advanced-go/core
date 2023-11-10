b5aa0626d9d48649a4660a41c607694a2ba6f684
go-ai-agent <mark.wilsonai@outlook.com>
2023-11-10 07:10:11 -0600

    Adding new packages

---
+++ io2/reader.go
@@ -0,0 +1,19 @@
+package io2
+
+import (
+	"github.com/go-ai-agent/core/runtime"
+	"io"
+)
+
+// ReadAll - read all the body, with a deferred close
+func ReadAll(body io.ReadCloser) ([]byte, *runtime.Status) {
+	if body == nil {
+		return nil, runtime.NewStatusOK()
+	}
+	defer body.Close()
+	buf, err := io.ReadAll(body)
+	if err != nil {
+		return nil, runtime.NewStatusError(runtime.StatusIOError, PkgUri+"/ReadAll", err)
+	}
+	return buf, runtime.NewStatusOK()
+}
---
+++ json2/marshal.go
@@ -0,0 +1,36 @@
+package json2
+
+import (
+	"encoding/json"
+	"github.com/go-ai-agent/core/runtime"
+)
+
+func Marshal(t any) ([]byte, *runtime.Status) {
+	buf, err := json.Marshal(t)
+	if err != nil {
+		return nil, runtime.NewStatusError(runtime.StatusJsonEncodeError, runtime.PkgUri+"/Marshal", err)
+	}
+	return buf, runtime.NewStatusOK()
+}
+
+func Unmarshal(buf []byte, t any) *runtime.Status {
+	err := json.Unmarshal(buf, t)
+	if err != nil {
+		return runtime.NewStatusError(runtime.StatusJsonDecodeError, runtime.PkgUri+"/Unmarshal", err)
+	}
+	return runtime.NewStatusOK()
+}
+
+/*
+func Unmarshal[T any](buf []byte) (T, *runtime.Status) {
+	var t T
+
+	err := json.Unmarshal(buf, &t)
+	if err != nil {
+		return t, runtime.NewStatusError(runtime.StatusJsonDecodeError, runtime.PkgUri+"/UnmarshalType", err)
+	}
+	return t, runtime.NewStatusOK()
+}
+
+
+*/
