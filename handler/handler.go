7a3e5d0ae63336873a030520ef40c6f548594a98
go-ai-agent <mark.wilsonai@outlook.com>
2023-11-09 18:43:31 -0600

    Adding generic access logging function

--- handler/handler.go
+++ handler/handler.go
@@ -11,14 +11,15 @@ import (
 // Configure as last handler in chain
 //middleware2.ControllerHttpHostMetricsHandler(mux, ""), status
 
-// HttpHostMetricsHandler - handler that applies access logging
-func HttpHostMetricsHandler(appHandler http.Handler, msg string) http.Handler {
+// HttpHostMetricsHandler - handler for Http request metrics
+func HttpHostMetricsHandler[T log.AccessHandler](appHandler http.Handler, msg string) http.Handler {
 	wrappedH := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now().UTC()
 		r = httpx.UpdateHeadersAndContext(r)
 		m := httpsnoop.CaptureMetrics(appHandler, w, r)
+		var t T
 		// log.Printf("%s %s (code=%d dt=%s written=%d)", r.Method, r.URL, m.Code, m.Duration, m.Written)
-		log.IngressAccess(start, time.Since(start), r, &http.Response{StatusCode: m.Code, ContentLength: m.Written}, -1, "")
+		t(log.IngressTraffic, start, time.Since(start), r, &http.Response{StatusCode: m.Code, ContentLength: m.Written}, -1, "")
 	})
 	return wrappedH
 }
--- log/z_package.go
+++ log/z_package.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/go-ai-agent/core/runtime"
-	"github.com/go-ai-agent/core/runtime/startup"
 	"net/http"
 	"reflect"
 	"time"
@@ -18,16 +17,19 @@ const (
 	IngressTraffic  = "ingress"
 )
 
+// AccessHandler - access logging handler
+type AccessHandler func(traffic string, start time.Time, duration time.Duration, req *http.Request, resp *http.Response, threshold int, statusFlags string)
+
 var (
 	PkgUri   = reflect.TypeOf(any(pkg{})).PkgPath()
-	accessFn startup.AccessLogFn
+	accessFn AccessHandler
 )
 
-func Access() startup.AccessLogFn {
+func GetAccessHandler() AccessHandler {
 	return accessFn
 }
 
-func SetAccess(fn startup.AccessLogFn) {
+func SetAccessHandler(fn AccessHandler) {
 	if fn != nil {
 		accessFn = fn
 	}
@@ -44,21 +46,15 @@ var defaultLogFn = func(traffic string, start time.Time, duration time.Duration,
 }
 
 func EgressAccess(start time.Time, duration time.Duration, req *http.Request, resp *http.Response, threshold int, statusFlags string) {
-	if accessFn != nil {
-		defaultLogFn(EgressTraffic, start, duration, req, resp, threshold, statusFlags)
-	}
+	AnyAccess(EgressTraffic, start, duration, req, resp, threshold, statusFlags)
 }
 
 func IngressAccess(start time.Time, duration time.Duration, req *http.Request, resp *http.Response, threshold int, statusFlags string) {
-	if accessFn != nil {
-		defaultLogFn(IngressTraffic, start, duration, req, resp, threshold, statusFlags)
-	}
+	AnyAccess(IngressTraffic, start, duration, req, resp, threshold, statusFlags)
 }
 
 func InternalAccess(start time.Time, duration time.Duration, req *http.Request, resp *http.Response, threshold int, statusFlags string) {
-	if accessFn != nil {
-		defaultLogFn(InternalTraffic, start, duration, req, resp, threshold, statusFlags)
-	}
+	AnyAccess(InternalTraffic, start, duration, req, resp, threshold, statusFlags)
 }
 
 // AnyAccess - needed for packages that have optional logging when core logging is not configured.
@@ -68,16 +64,17 @@ func AnyAccess(traffic string, start time.Time, duration time.Duration, req *htt
 	}
 }
 
-// Wrap - wrap a DoHandler with access logging
-func Wrap(handler runtime.DoHandler) runtime.DoHandler {
+// WrapDo - wrap a DoHandler with access logging
+func WrapDo[T AccessHandler](handler runtime.DoHandler) runtime.DoHandler {
 	return func(ctx any, req *http.Request, body any) (any, *runtime.Status) {
 		var start = time.Now().UTC()
 
 		if handler == nil {
-			return nil, runtime.NewStatusError(runtime.StatusInvalidArgument, PkgUri+"/Apply", errors.New("error:Do handler function is nil for access log")).SetRequestId(req.Context())
+			return nil, runtime.NewStatusError(runtime.StatusInvalidArgument, PkgUri+"/Wrap", errors.New("error:Do handler function is nil for access log")).SetRequestId(req.Context())
 		}
-		t, status := handler(ctx, req, body)
-		InternalAccess(start, time.Since(start), req, &http.Response{StatusCode: status.Code()}, -1, "")
-		return t, status
+		var t T
+		data, status := handler(ctx, req, body)
+		t(InternalTraffic, start, time.Since(start), req, &http.Response{StatusCode: status.Code()}, -1, "")
+		return data, status
 	}
 }
