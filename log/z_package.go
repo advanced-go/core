be7b08809ed85bec157408ca3f51b5c2676075fe
go-ai-agent <mark.wilsonai@outlook.com>
2023-11-09 18:17:03 -0600

    Refactoring

--- log/z_package.go
+++ log/z_package.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"errors"
 	"fmt"
 	"github.com/go-ai-agent/core/runtime"
 	"github.com/go-ai-agent/core/runtime/startup"
@@ -64,5 +65,19 @@ func InternalAccess(start time.Time, duration time.Duration, req *http.Request, 
 func AnyAccess(traffic string, start time.Time, duration time.Duration, req *http.Request, resp *http.Response, threshold int, statusFlags string) {
 	if accessFn != nil {
 		defaultLogFn(traffic, start, duration, req, resp, threshold, statusFlags)
+	}
+}
+
+// Wrap - wrap a DoHandler with access logging
+func Wrap(handler runtime.DoHandler) runtime.DoHandler {
+	return func(ctx any, req *http.Request, body any) (any, *runtime.Status) {
+		var start = time.Now().UTC()
+
+		if handler == nil {
+			return nil, runtime.NewStatusError(runtime.StatusInvalidArgument, PkgUri+"/Apply", errors.New("error:Do handler function is nil for access log")).SetRequestId(req.Context())
+		}
+		t, status := handler(ctx, req, body)
+		InternalAccess(start, time.Since(start), req, &http.Response{StatusCode: status.Code()}, -1, "")
+		return t, status
 	}
 }
