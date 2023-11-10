c29dc5db2211f1771b50030b0d06150b85a42249
go-ai-agent <mark.wilsonai@outlook.com>
2023-11-10 09:43:08 -0600

    Update request_test.go

--- http2/request_test.go
+++ http2/request_test.go
@@ -3,15 +3,15 @@ package http2
 import (
 	"context"
 	"fmt"
-	"github.com/go-ai-agent/core/log"
+	"github.com/go-ai-agent/core/log2"
 	"github.com/go-ai-agent/core/runtime"
 	"net/http"
 )
 
 func Example_NewRequest_Nil() {
 	newReq, status := NewRequest(nil, "get", "https://www/google.com/search?q=golang", "variant:location")
-	fn := log.GetAccessHandler()
-	fmt.Printf("test: NewRequest(nil) -> [status:%v] [req-len:%v] [ctx-len:%v] [access:%v] [var:%v]\n", status, len(runtime.RequestId(newReq)), len(runtime.RequestId(newReq.Context())), fn != nil, newReq.Header.Get(runtime.ContentLocation))
+	fn := log2.GetAccessHandler()
+	fmt.Printf("test: NewRequest(nil) -> [status:%v] [req-len:%v] [ctx-len:%v] [access:%v] [var:%v]\n", status, len(runtime.RequestId(newReq)), len(runtime.RequestId(newReq.Context())), fn != nil, newReq.Header.Get(ContentLocation))
 
 	//Output:
 	//test: NewRequest(nil) -> [status:OK] [req-len:36] [ctx-len:36] [access:true] [var:variant:location]
@@ -21,13 +21,13 @@ func Example_NewRequest_Nil() {
 func Example_NewRequest_Context() {
 	ctx := context.Background()
 	newReq, status := NewRequest(ctx, "get", "https://www/google.com/search?q=golang", "variant:location-2")
-	fn := log.GetAccessHandler()
-	fmt.Printf("test: NewRequest(ctx) -> [status:%v] [req-len:%v] [ctx-len:%v] [access:%v] [var:%v]\n", status, len(runtime.RequestId(newReq)), len(runtime.RequestId(newReq.Context())), fn != nil, newReq.Header.Get(runtime.ContentLocation))
+	fn := log2.GetAccessHandler()
+	fmt.Printf("test: NewRequest(ctx) -> [status:%v] [req-len:%v] [ctx-len:%v] [access:%v] [var:%v]\n", status, len(runtime.RequestId(newReq)), len(runtime.RequestId(newReq.Context())), fn != nil, newReq.Header.Get(ContentLocation))
 
 	ctx = runtime.NewRequestIdContext(context.Background(), "123456")
 	newReq, status = NewRequest(ctx, "get", "https://www/google.com/search?q=golang", "variant:location")
-	fn = log.GetAccessHandler()
-	fmt.Printf("test: NewRequest(ctx) -> [status:%v] [req-len:%v] [ctx-len:%v] [access:%v] [var:%v]\n", status, len(runtime.RequestId(newReq)), len(runtime.RequestId(newReq.Context())), fn != nil, newReq.Header.Get(runtime.ContentLocation))
+	fn = log2.GetAccessHandler()
+	fmt.Printf("test: NewRequest(ctx) -> [status:%v] [req-len:%v] [ctx-len:%v] [access:%v] [var:%v]\n", status, len(runtime.RequestId(newReq)), len(runtime.RequestId(newReq.Context())), fn != nil, newReq.Header.Get(ContentLocation))
 
 	//Output:
 	//test: NewRequest(ctx) -> [status:OK] [req-len:36] [ctx-len:36] [access:true] [var:variant:location-2]
@@ -40,14 +40,14 @@ func Example_NewRequest_Request() {
 
 	req, _ := http.NewRequest("", "https://www/google.com/search?q=golang", nil)
 	newReq, status := NewRequest(req, "get", "https://www/google.com/search?q=golang", "variant:location-2")
-	fn := log.GetAccessHandler()
-	fmt.Printf("test: NewRequest(ctx) -> [status:%v] [req-len:%v] [ctx-len:%v] [access:%v] [var:%v]\n", status, len(runtime.RequestId(newReq)), len(runtime.RequestId(newReq.Context())), fn != nil, newReq.Header.Get(runtime.ContentLocation))
+	fn := log2.GetAccessHandler()
+	fmt.Printf("test: NewRequest(ctx) -> [status:%v] [req-len:%v] [ctx-len:%v] [access:%v] [var:%v]\n", status, len(runtime.RequestId(newReq)), len(runtime.RequestId(newReq.Context())), fn != nil, newReq.Header.Get(ContentLocation))
 
 	req, _ = http.NewRequest("", "https://www/google.com/search?q=golang", nil)
 	req.Header.Add(runtime.XRequestId, "1234-5678")
 	newReq, status = NewRequest(req, "get", "https://www/google.com/search?q=golang", "variant:location-3")
-	fn = log.GetAccessHandler()
-	fmt.Printf("test: NewRequest(ctx) -> [status:%v] [req-len:%v] [ctx-len:%v] [access:%v] [var:%v]\n", status, len(runtime.RequestId(newReq)), len(runtime.RequestId(newReq.Context())), fn != nil, newReq.Header.Get(runtime.ContentLocation))
+	fn = log2.GetAccessHandler()
+	fmt.Printf("test: NewRequest(ctx) -> [status:%v] [req-len:%v] [ctx-len:%v] [access:%v] [var:%v]\n", status, len(runtime.RequestId(newReq)), len(runtime.RequestId(newReq.Context())), fn != nil, newReq.Header.Get(ContentLocation))
 
 	//Output:
 	//test: NewRequest(ctx) -> [status:OK] [req-len:36] [ctx-len:36] [access:true] [var:variant:location-2]
