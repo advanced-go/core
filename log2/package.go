e49600364228f78e57d496677166fdf2f04c4833
go-ai-agent <mark.wilsonai@outlook.com>
2023-11-12 03:00:09 -0600

    Updating

--- log2/package.go
+++ log2/package.go
@@ -1,6 +1,7 @@
 package log2
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"github.com/go-ai-agent/core/runtime"
@@ -94,7 +95,7 @@ func WrapDo(handler runtime.DoHandler) runtime.DoHandler {
 
 // WrapHttp - wrap a HttpHandler with access logging
 func WrapHttp(handler runtime.HttpHandler) runtime.HttpHandler {
-	return func(ctx any, w http.ResponseWriter, r *http.Request) *runtime.Status {
+	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) *runtime.Status {
 		var start = time.Now().UTC()
 
 		if handler == nil {
--- runtime/handler.go
+++ runtime/handler.go
@@ -1,11 +1,12 @@
 package runtime
 
 import (
+	"context"
 	"net/http"
 )
 
 // HttpHandler - function type for HTTP handling
-type HttpHandler func(ctx any, w http.ResponseWriter, r *http.Request) *Status
+type HttpHandler func(ctx context.Context, w http.ResponseWriter, r *http.Request) *Status
 
 // DoHandler - function type for a Do handler
 type DoHandler func(ctx any, r *http.Request, body any) (any, *Status)
