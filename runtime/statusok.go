788612399f81bc95b39774289badc8266b09659a
go-ai-agent <mark.wilsonai@outlook.com>
2023-11-17 12:58:33 -0600

    Update statusok.go

--- runtime/statusok.go
+++ runtime/statusok.go
@@ -25,17 +25,17 @@ func (s *statusOK) FirstError() error { return nil }
 func (s *statusOK) Duration() time.Duration            { return 0 }
 func (s *statusOK) SetDuration(_ time.Duration) Status { return s }
 
-func (s *statusOK) RequestId() string                 { return "" }
-func (s *statusOK) SetRequestId(requestId any) Status { return s }
+func (s *statusOK) RequestId() string         { return "" }
+func (s *statusOK) SetRequestId(_ any) Status { return s }
 
 func (s *statusOK) Location() []string          { return nil }
 func (s *statusOK) AddLocation(_ string) Status { return s }
 
-func (s *statusOK) IsContent() bool                                 { return false }
-func (s *statusOK) Content() any                                    { return nil }
-func (s *statusOK) ContentHeader() http.Header                      { return nil }
-func (s *statusOK) ContentString() string                           { return "" }
-func (s *statusOK) SetContent(content any, jsonContent bool) Status { return s }
+func (s *statusOK) IsContent() bool                 { return false }
+func (s *statusOK) Content() any                    { return nil }
+func (s *statusOK) ContentHeader() http.Header      { return nil }
+func (s *statusOK) ContentString() string           { return "" }
+func (s *statusOK) SetContent(_ any, _ bool) Status { return s }
 
 func (s *statusOK) Description() string { return "OK" }
 func (s *statusOK) String() string      { return s.Description() }
