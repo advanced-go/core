5502a65ba676b1256ceff3286571e82318a92b22
go-ai-agent <mark.wilsonai@outlook.com>
2023-11-19 12:03:15 -0600

    Refactoing

--- http2/content.go
+++ http2/content.go
@@ -4,16 +4,14 @@ import (
 	"errors"
 	"github.com/advanced-go/core/io2"
 	"github.com/advanced-go/core/runtime"
-	"net/http"
 	"net/url"
 )
 
-func ReadContentFromLocation(h http.Header) ([]byte, runtime.Status) {
-	content := h.Get(ContentLocation)
-	if len(content) == 0 {
+func ReadContentFromLocation(location string) ([]byte, runtime.Status) {
+	if len(location) == 0 {
 		return nil, runtime.NewStatusError(runtime.StatusInvalidContent, "ReadContentFromLocation()", errors.New("error: content-location is empty"))
 	}
-	u, _ := url.Parse(content)
+	u, _ := url.Parse(location)
 	buf, err := io2.ReadFile(u)
 	if err != nil {
 		return nil, runtime.NewStatusError(runtime.StatusInvalidContent, "ReadContentFromLocation()", err)
--- http2/content_test.go
+++ http2/content_test.go
@@ -2,14 +2,10 @@ package http2
 
 import (
 	"fmt"
-	"net/http"
 )
 
 func Example_ReadContentFromLocation() {
-	h := make(http.Header)
-	h.Add(ContentLocation, "file://[cwd]/http2test/resource/activity.json")
-	buf, status := ReadContentFromLocation(h)
-
+	buf, status := ReadContentFromLocation("file://[cwd]/http2test/resource/activity.json")
 	fmt.Printf("test: ReadContentFromLocation() -> [status:%v] [content:%v]\n", status, len(buf))
 
 	//Output:
