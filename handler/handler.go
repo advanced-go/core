8d53175f79cbd231d386d2df3ac56d176d4d70b2
go-ai-agent <mark.wilsonai@outlook.com>
2023-11-26 10:57:18 -0600

    Update handler.go

--- handler/handler.go
+++ handler/handler.go
@@ -17,7 +17,7 @@ func HttpHostMetricsHandler(appHandler http.Handler, msg string) http.Handler {
 		access.AddRequestId(r)
 		m := httpsnoop.CaptureMetrics(appHandler, w, r)
 		// log.Printf("%s %s (code=%d dt=%s written=%d)", r.Method, r.URL, m.Code, m.Duration, m.Written)
-		access.LogIngress(start, time.Since(start), r, &http.Response{StatusCode: m.Code, ContentLength: m.Written}, -1, "")
+		access.Log(access.IngressTraffic, start, time.Since(start), r, &http.Response{StatusCode: m.Code, ContentLength: m.Written}, -1, "")
 	})
 	return wrappedH
 }
