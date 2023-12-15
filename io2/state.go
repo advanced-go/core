879ccca12094cd3a278de5aec1d72045b36427b4
go-ai-agent <mark.wilsonai@outlook.com>
2023-12-15 12:08:51 -0600

    Adding multiple URL's

--- io2/state.go
+++ io2/state.go
@@ -8,7 +8,8 @@ import (
 )
 
 const (
-	readStateLoc = PkgPath + ":ReadState"
+	readStateLoc   = PkgPath + ":ReadState"
+	readResultsLoc = PkgPath + ":ReadResults"
 )
 
 func ReadState[T any](uri string) (t T, status runtime.Status) {
@@ -28,4 +29,24 @@ func ReadState[T any](uri string) (t T, status runtime.Status) {
 		return t, runtime.NewStatusError(runtime.StatusJsonDecodeError, readStateLoc, err)
 	}
 	return t, runtime.StatusOK()
+}
+
+func ReadResults[T any](urls []string) (t T, status runtime.Status) {
+	switch len(urls) {
+	case 0:
+		return t, runtime.StatusOK()
+	case 1:
+		if len(urls[0]) == 0 {
+			return t, runtime.StatusOK()
+		}
+		return ReadState[T](urls[0])
+	default:
+	}
+	if len(urls[0]) > 0 {
+		t, status = ReadState[T](urls[0])
+		if !status.OK() {
+			return t, status
+		}
+	}
+	return t, ReadStatus(urls[1])
 }
--- io2/state_test.go
+++ io2/state_test.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+const (
+	addressUrl   = "file://[cwd]/io2test/resource/address.json"
+	status504Url = "file://[cwd]/io2test/resource/status-504.json"
+)
+
 type address struct {
 	City    string
 	State   string
@@ -11,11 +16,39 @@ type address struct {
 }
 
 func ExampleReadState() {
-	uri := "file://[cwd]/io2test/resource/address.json"
-	t, status := ReadState[address](uri)
+	t, status := ReadState[address](addressUrl)
 	fmt.Printf("test: ReadState() -> [address:%v] [status:%v]\n", t, status)
 
 	//Output:
 	//test: ReadState() -> [address:{frisco texas 75034}] [status:OK]
+
+}
+
+func ExampleReadResults() {
+	t, status := ReadResults[address](nil)
+	fmt.Printf("test: ReadResults(nil) -> [state:%v] [status:%v]\n", t, status)
+
+	t, status = ReadResults[address]([]string{""})
+	fmt.Printf("test: ReadResults(nil) -> [state:%v] [status:%v]\n", t, status)
+
+	t, status = ReadResults[address]([]string{"", ""})
+	fmt.Printf("test: ReadResults(nil) -> [state:%v] [status:%v]\n", t, status)
+
+	t, status = ReadResults[address]([]string{addressUrl})
+	fmt.Printf("test: ReadResults(nil) -> [state:%v] [status:%v]\n", t, status)
+
+	t, status = ReadResults[address]([]string{addressUrl, ""})
+	fmt.Printf("test: ReadResults(nil) -> [state:%v] [status:%v]\n", t, status)
+
+	t, status = ReadResults[address]([]string{addressUrl, status504Url})
+	fmt.Printf("test: ReadResults(nil) -> [state:%v] [status:%v]\n", t, status)
+
+	//Output:
+	//test: ReadResults(nil) -> [state:{  }] [status:OK]
+	//test: ReadResults(nil) -> [state:{  }] [status:OK]
+	//test: ReadResults(nil) -> [state:{  }] [status:OK]
+	//test: ReadResults(nil) -> [state:{frisco texas 75034}] [status:OK]
+	//test: ReadResults(nil) -> [state:{frisco texas 75034}] [status:OK]
+	//test: ReadResults(nil) -> [state:{frisco texas 75034}] [status:Timeout [error 1]]
 
 }
