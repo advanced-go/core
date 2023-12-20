1cf6c6d67e6677fe24662312e586b4f8bd14bb2e
go-ai-agent <mark.wilsonai@outlook.com>
2023-12-20 14:11:11 -0600

    Adding lookup interface

--- runtime/lookup.go
+++ runtime/lookup.go
@@ -5,10 +5,43 @@ import (
 	"reflect"
 )
 
+//https://stackoverflow.com/questions/72034479/how-to-implement-generic-interfaces
+
 const (
 	stringValueError = "error: stringFromType() value parameter is nil"
 	listValueError   = "error: listFromType() value parameter is nil"
 )
+
+// LookupResultConstraints - lookup function constraints
+type LookupResultConstraints interface {
+	string | []string
+}
+
+type Lookup[T LookupResultConstraints] interface {
+	Resolve(key string) (string, bool)
+	SetOverride(t any)
+}
+
+type lookup[F LookupFunctionConstraints] struct {
+	overrideFn F
+	defaultFn  F
+}
+
+func (l *lookup[F]) SetOverride(t any) {
+	if t == nil {
+		l.overrideFn = nil
+	}
+}
+
+func (l *lookup[F]) Resolve(key string) (string, bool) {
+	return key, false
+}
+
+func NewLookup[T LookupResultConstraints, F LookupFunctionConstraints](defaultFn F) Lookup[T] {
+	l := new(lookup[F])
+	l.defaultFn = defaultFn
+	return l
+}
 
 // LookupFunctionConstraints - lookup function constraints
 type LookupFunctionConstraints interface {
