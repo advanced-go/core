33cab58e927d11e770a75dbd2b1e4646ca66fc98
go-ai-agent <mark.wilsonai@outlook.com>
2023-12-22 14:03:12 -0600

    Updating

--- handler/handler.go
+++ handler/handler.go
@@ -14,10 +14,10 @@ import (
 func HttpHostMetricsHandler(appHandler http.Handler, msg string) http.Handler {
 	wrappedH := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now().UTC()
-		access.AddRequestId(r)
+		//access.AddRequestId(r)
 		m := httpsnoop.CaptureMetrics(appHandler, w, r)
 		// log.Printf("%s %s (code=%d dt=%s written=%d)", r.Method, r.URL, m.Code, m.Duration, m.Written)
-		access.Log(access.IngressTraffic, start, time.Since(start), r, &http.Response{StatusCode: m.Code, ContentLength: m.Written}, "", -1, "")
+		access.Log(access.IngressTraffic, start, time.Since(start), r, &http.Response{StatusCode: m.Code, ContentLength: m.Written}, "", "", -1, "")
 	})
 	return wrappedH
 }
---
+++ handler/handler_test.go
@@ -0,0 +1,11 @@
+package handler
+
+import "fmt"
+
+func Example_Handler() {
+	fmt.Printf("test: Handler() -> %v\n", "test")
+
+	//Output:
+	//test: Handler() -> test
+	
+}
