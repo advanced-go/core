0cae8546063e8638aa5542793818896c2ed73746
go-ai-agent <mark.wilsonai@outlook.com>
2023-12-30 05:23:32 -0600

    Update new_test.go

--- runtime/new_test.go
+++ runtime/new_test.go
@@ -59,31 +59,56 @@ func ExampleNew_URLError() {
 }
 
 func ExampleNew() {
-	addr, status := New[address2](status504)
-	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", address1Url, addr, status)
+	s := status504
+	addr, status := New[address2](s)
+	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", s, addr, status)
 
-	addr, status = New[address2](address1Url)
-	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", address1Url, addr, status)
+	s = address1Url
+	addr, status = New[address2](s)
+	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", s, addr, status)
 
-	addr, status = New[address2](uri.ParseRaw(status504))
-	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", address1Url, addr, status)
+	s = status504
+	addr, status = New[address2](uri.ParseRaw(s))
+	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", s, addr, status)
 
-	addr, status = New[address2](uri.ParseRaw(address1Url))
-	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", address1Url, addr, status)
+	s = address1Url
+	addr, status = New[address2](uri.ParseRaw(s))
+	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", s, addr, status)
 
-	buf, err := os.ReadFile(uri.FileName(address2Url))
+	s = address2Url
+	buf, err := os.ReadFile(uri.FileName(s))
 	if err != nil {
 		fmt.Printf("test: os.ReadFile() -> [err:%v]\n", err)
 	}
 	body := io.NopCloser(strings.NewReader(string(buf)))
 	addr, status = New[address2](body)
-	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", address2Url, addr, status)
+	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", s, addr, status)
 
 	//Output:
-	//test: New(file://[cwd]/runtimetest/address1.json) -> [addr:{  }] [status:Timeout [error 1]]
+	//test: New(file://[cwd]/runtimetest/status-504.json) -> [addr:{  }] [status:Timeout [error 1]]
 	//test: New(file://[cwd]/runtimetest/address1.json) -> [addr:{frisco texas 75034}] [status:OK]
-	//test: New(file://[cwd]/runtimetest/address1.json) -> [addr:{  }] [status:Timeout [error 1]]
+	//test: New(file://[cwd]/runtimetest/status-504.json) -> [addr:{  }] [status:Timeout [error 1]]
 	//test: New(file://[cwd]/runtimetest/address1.json) -> [addr:{frisco texas 75034}] [status:OK]
 	//test: New(file://[cwd]/runtimetest/address2.json) -> [addr:{vinton iowa 52349}] [status:OK]
+
+}
+
+func ExampleNew_Const_Status() {
+	s := StatusOKUri
+	addr, status := New[address2](s)
+	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", s, addr, status)
+
+	s = StatusNotFoundUri
+	addr, status = New[address2](s)
+	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", s, addr, status)
+
+	s = StatusTimeoutUri
+	addr, status = New[address2](s)
+	fmt.Printf("test: New(%v) -> [addr:%v] [status:%v]\n", s, addr, status)
+
+	//Output:
+	//test: New(urn:status:ok) -> [addr:{  }] [status:OK]
+	//test: New(urn:status:notfound) -> [addr:{  }] [status:Not Found]
+	//test: New(urn:status:timeout) -> [addr:{  }] [status:Timeout]
 
 }
