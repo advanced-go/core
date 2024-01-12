2433f1411456d22f3030586630aa56391125b52a
go-ai-agent <mark.wilsonai@outlook.com>
2024-01-12 07:09:55 -0600

    Testing

--- exchange/get.go
+++ exchange/get.go
@@ -9,18 +9,12 @@ const (
 	getLocation = PkgPath + ":Get"
 )
 
-func Get[E runtime.ErrorHandler](uri string, h http.Header, callerLocation string) (*http.Response, runtime.Status) {
-	var e E
-
+func Get(uri string, h http.Header) (*http.Response, runtime.Status) {
 	req, err := http.NewRequest(http.MethodGet, uri, nil)
 	if err != nil {
-		return nil, e.Handle(runtime.NewStatusError(http.StatusInternalServerError, getLocation, err), runtime.RequestId(h), callerLocation)
+		return nil, runtime.NewStatusError(http.StatusBadRequest, getLocation, err)
 	}
 	req.Header = h
 	// exchange.Do() will always return a non nil *http.Response
-	resp, status := Do(req)
-	if !status.OK() {
-		return nil, e.Handle(status, runtime.RequestId(h), callerLocation)
-	}
-	return resp, runtime.StatusOK()
+	return Do(req)
 }
--- exchange/get_test.go
+++ exchange/get_test.go
@@ -1,1 +1,13 @@
 package exchange
+
+import "fmt"
+
+func ExampleGet() {
+	r, status := Get("", nil)
+
+	fmt.Printf("test: Get(\"\") -> [resp:%v] [status:%v]\n", r.Status, status)
+
+	//Output:
+	//test: Get("") -> [resp:Internal Error] [status:Internal Error [Get "": unsupported protocol scheme ""]]
+
+}
