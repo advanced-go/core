3438757bd79635da692f361b0103cbf7f47b502e
go-ai-agent <mark.wilsonai@outlook.com>
2024-01-12 11:14:33 -0600

    Removing test cases

--- uri/resolver_test.go
+++ uri/resolver_test.go
@@ -201,28 +201,3 @@ func Example_Values() {
 	//test: Values.Encode() -> param-1=value-1&param-2=value-2
 
 }
-
-/*
-func Example_Attr() {
-	values := []runtime.Pari{{"MSFT", MSFTAuthority}, {"GOOGL", GOOGLAuthority}}
-
-	buf, err := json.Marshal(values)
-	fmt.Printf("test: Attr() -> [buf:%v] [err:%v]\n", string(buf), err)
-
-	fname := FileName(fileAttrs)
-	buf, err = os.ReadFile(fname)
-	fmt.Printf("test: os.ReadFile(%v) -> [buf:%v] [err:%v]\n", fname, len(buf), err)
-	var values2 []runtime.Pari
-
-	err = json.Unmarshal(buf, &values2)
-	fmt.Printf("test: Unmarshal() -> [buf:%v] [err:%v]\n", values2, err)
-
-	//Output:
-	//test: Attr() -> [buf:[{"Key":"MSFT","Value":"www.bing.com"},{"Key":"GOOGL","Value":"www.google.com"}]] [err:<nil>]
-	//test: os.ReadFile(C:\Users\markb\GitHub\core\uri\uritest\attrs.json) -> [buf:124] [err:<nil>]
-	//test: Unmarshal() -> [buf:[{MSFT www.bing.com} {GOOGL www.google.com}]] [err:<nil>]
-
-}
-
-
-*/
