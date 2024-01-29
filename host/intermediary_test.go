ffafc2e9726b37561e5045b6d818cb5f95cb0f81
go-ai-agent <mark.wilsonai@outlook.com>
2024-01-29 14:33:20 -0600

    Update intermediary_test.go

--- host/intermediary_test.go
+++ host/intermediary_test.go
@@ -7,17 +7,16 @@ import (
 	"net/http/httptest"
 )
 
-const ()
-
 type authComponent struct {
 }
 
 func (ac *authComponent) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	tokenString := r.Header.Get(Authorization)
-	if tokenString == "" {
-		w.WriteHeader(http.StatusUnauthorized)
-		fmt.Fprint(w, "Missing authorization header")
-		return
+	if r != nil {
+		tokenString := r.Header.Get(Authorization)
+		if tokenString == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Missing authorization header")
+		}
 	}
 }
 
@@ -27,6 +26,39 @@ type serviceComponent struct {
 func (ac *serviceComponent) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "Service OK")
+}
+
+func ExampleIntermediary_Nil() {
+	auth := new(authComponent)
+	serv := new(serviceComponent)
+
+	ic := NewIntermediary(nil, nil)
+	rec := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "https://www.google.com/search?q-golang", nil)
+	ic.ServeHTTP(rec, r)
+	buf, _ := runtime.ReadAll(rec.Result().Body, nil)
+	fmt.Printf("test: ServeHTTP()-nil-components -> [status-code:%v] [content:%v]\n", rec.Result().StatusCode, string(buf))
+
+	ic = NewIntermediary(nil, serv)
+	rec = httptest.NewRecorder()
+	r, _ = http.NewRequest(http.MethodGet, "https://www.google.com/search?q-golang", nil)
+	ic.ServeHTTP(rec, r)
+	buf, _ = runtime.ReadAll(rec.Result().Body, nil)
+	fmt.Printf("test: ServeHTTP()-service-only -> [status-code:%v] [content:%v]\n", rec.Result().StatusCode, string(buf))
+
+	ic = NewIntermediary(auth, serv)
+	rec = httptest.NewRecorder()
+	r, _ = http.NewRequest(http.MethodGet, "https://www.google.com/search?q-golang", nil)
+	r.Header.Add(Authorization, "token")
+	ic.ServeHTTP(rec, r)
+	buf, _ = runtime.ReadAll(rec.Result().Body, nil)
+	fmt.Printf("test: ServeHTTP()-auth-only -> [status-code:%v] [content:%v]\n", rec.Result().StatusCode, string(buf))
+
+	//Output:
+	//test: ServeHTTP()-nil-components -> [status-code:200] [content:]
+	//test: ServeHTTP()-service-only -> [status-code:200] [content:Service OK]
+	//test: ServeHTTP()-auth-only -> [status-code:200] [content:Service OK]
+	
 }
 
 func ExampleIntermediary_ServeHTTP() {
