03d03e195a00e92016dee8cfc811fd0d649f8c31
advanced-go <mark.wilson.go@outlook.com>
2024-03-22 06:59:36 -0500

    Adding attributes

--- runtime/error.go
+++ runtime/error.go
@@ -149,47 +149,6 @@ func formatErrors(name string, errs []error) string {
 	return result + " ]"
 }
 
-func formatAttrs(attrs []any) string {
-	if len(attrs) == 0 || attrs[0] == nil {
-		return ""
-	}
-	result := ""
-	value := ""
-	name := ""
-	for i := 0; i < len(attrs)-1; i += 2 {
-		if i != 0 {
-			result += ", "
-		}
-		name = fmt.Sprintf("%v", attrs[i])
-		if n, ok := attrs[i+1].(int); ok {
-			value = strconv.Itoa(n)
-			result += jsonMarkup(name, value, false)
-			continue
-		}
-		if b, ok1 := attrs[i+1].(bool); ok1 {
-			if b {
-				value = "true"
-			} else {
-				value = "false"
-			}
-			result += jsonMarkup(name, value, false)
-			continue
-		}
-		if t, ok2 := attrs[i+1].(time.Time); ok2 {
-			result += jsonMarkup(name, FmtRFC3339Millis(t), true)
-			continue
-		}
-		value = fmt.Sprintf("%v", attrs[i+1])
-		result += jsonMarkup(name, value, true)
-	}
-	if (len(attrs) & 0x01) == 1 {
-		result += ", "
-		name = fmt.Sprintf("%v", attrs[len(attrs)-1])
-		result += jsonMarkup(name, "", true)
-	}
-	return result
-}
-
 // OutputFormatter - formatter for special output formatting
 func OutputFormatter(ts time.Time, code int, status, requestId string, errs []error, trace []string) string {
 	str := strconv.Itoa(code)
