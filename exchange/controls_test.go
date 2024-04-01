fc6a9e7cf05ea6fc573d2acb590e647ee3487e34
advanced-go <mark.wilson.go@outlook.com>
2024-04-01 08:02:22 -0500

    Adding controls map

---
+++ exchange/controls_test.go
@@ -0,0 +1,70 @@
+package exchange
+
+import (
+	"fmt"
+	"github.com/advanced-go/core/controller"
+	"time"
+)
+
+var ctrl = controller.NewController("test-route", time.Second)
+
+func ExampleControls_Add() {
+	p := NewControls()
+	path := "http://localhost:8080/github.com/advanced-go/example-domain/activity"
+
+	status := p.Register("", nil)
+	fmt.Printf("test: Register(\"\") -> [status:%v]\n", status)
+
+	status = p.Register(path, nil)
+	fmt.Printf("test: Register(%v) -> [status:%v]\n", path, status)
+
+	status = p.Register(path, ctrl)
+	fmt.Printf("test: Register(%v) -> [status:%v]\n", path, status)
+
+	status = p.Register(path, ctrl)
+	fmt.Printf("test: Register(%v) -> [status:%v]\n", path, status)
+
+	path = "http://localhost:8080/github/advanced-go/example-domain/activity"
+	status = p.Register(path, ctrl)
+	fmt.Printf("test: Register(%v) -> [status:%v]\n", path, status)
+
+	//Output:
+	//test: Register("") -> [status:Invalid Argument [invalid argument: path is empty]]
+	//test: Register(http://localhost:8080/github.com/advanced-go/example-domain/activity) -> [status:Invalid Argument [invalid argument: Controller is nil: [http://localhost:8080/github.com/advanced-go/example-domain/activity]]]
+	//test: Register(http://localhost:8080/github.com/advanced-go/example-domain/activity) -> [status:OK]
+	//test: Register(http://localhost:8080/github.com/advanced-go/example-domain/activity) -> [status:Invalid Argument [invalid argument: Controller already exists: [http://localhost:8080/github.com/advanced-go/example-domain/activity]]]
+	//test: Register(http://localhost:8080/github/advanced-go/example-domain/activity) -> [status:OK]
+
+}
+
+func ExampleControls_Get() {
+	p := NewControls()
+	path := "http://localhost:8080/github.com/advanced-go/example-domain/activity"
+
+	_, status := p.Lookup("")
+	fmt.Printf("test: Lookup(\"\") -> [status:%v]\n", status)
+
+	_, status = p.Lookup(path)
+	fmt.Printf("test: Lookup(%v) -> [status:%v]\n", path, status)
+
+	status = p.Register(path, ctrl)
+	fmt.Printf("test: Register(%v) -> [status:%v]\n", path, status)
+
+	handler, status1 := p.Lookup(path)
+	fmt.Printf("test: Lookup(%v) -> [status:%v] [handler:%v]\n", path, status1, handler != nil)
+
+	path = "http://localhost:8080/github/advanced-go/example-domain/activity"
+	status = p.Register(path, ctrl)
+	fmt.Printf("test: Register(%v) -> [status:%v]\n", path, status)
+	handler, status1 = p.Lookup(path)
+	fmt.Printf("test: Lookup(%v) -> [status:%v] [handler:%v]\n", path, status1, handler != nil)
+
+	//Output:
+	//test: Lookup("") -> [status:Invalid Argument [invalid argument: path is invalid: []]]
+	//test: Lookup(http://localhost:8080/github.com/advanced-go/example-domain/activity) -> [status:Invalid Argument [invalid argument: Controller does not exist: [github.com/advanced-go/example-domain/activity]]]
+	//test: Register(http://localhost:8080/github.com/advanced-go/example-domain/activity) -> [status:OK]
+	//test: Lookup(http://localhost:8080/github.com/advanced-go/example-domain/activity) -> [status:OK] [handler:true]
+	//test: Register(http://localhost:8080/github/advanced-go/example-domain/activity) -> [status:OK]
+	//test: Lookup(http://localhost:8080/github/advanced-go/example-domain/activity) -> [status:OK] [handler:true]
+
+}
